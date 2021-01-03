3837483491f1df5a1ebd7db40163914254da798e
Dele <dadetifa1@yahoo.com>
2021-01-03 17:21:13 -0600

    merge conflit

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const errorHandler = require('./middleware/error-handler')
+const logger = require('./logger')
 const todoRouter = require('./Posting/Posting-router')
 const salepersonRouter = require('./Sales_person/sales-person-router')
 const salesCommissionRouter = require('./Sales_Commission/Sales-Commission-router')
@@ -22,6 +23,17 @@ app.use(cors())
 app.use(helmet())
 
 // app.use(express.static('public'))
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})  
 
 app.use('/api/postings', todoRouter)
 app.use('/api/salepeople', salepersonRouter)
